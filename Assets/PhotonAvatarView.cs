dd31b3d107d947dd8a0aab8afe633890804a89e0
Anjur <vanjur2@illinois.edu>
2017-11-01 16:23:00 -0500

    Initial File Upload

---
+++ Assets/PhotonAvatarView.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class PhotonAvatarView : MonoBehaviour {
+
+    private PhotonView photonView;
+    private OvrAvatar ovrAvatar;
+    private OvrAvatarRemoteDriver remoteDriver;
+
+    private List<byte[]> packetData;
+
+    public readonly byte InstantiateVrAvatarEventCode = 123;
+
+
+    public void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+        if (photonView.isMine)
+        {
+            ovrAvatar = GetComponent<OvrAvatar>();
+
+            packetData = new List<byte[]>();
+        }
+        else
+        {
+            remoteDriver = GetComponent<OvrAvatarRemoteDriver>();
+        }
+    }
+
+    public void OnEnable()
+    {
+        if (photonView.isMine)
+        {
+            ovrAvatar.RecordPackets = true;
+            ovrAvatar.PacketRecorded += OnLocalAvatarPacketRecorded;
+        }
+    }
+
+    public void OnDisable()
+    {
+        if (photonView.isMine)
+        {
+            ovrAvatar.RecordPackets = false;
+            ovrAvatar.PacketRecorded -= OnLocalAvatarPacketRecorded;
+        }
+    }
+
+    private int localSequence;
+
+    public void OnLocalAvatarPacketRecorded(object sender, OvrAvatar.PacketEventArgs args)
+    {
+        using (MemoryStream outputStream = new MemoryStream())
+        {
+            BinaryWriter writer = new BinaryWriter(outputStream);
+
+            var size = Oculus.Avatar.CAPI.ovrAvatarPacket_GetSize(args.Packet.ovrNativePacket);
+            byte[] data = new byte[size];
+            Oculus.Avatar.CAPI.ovrAvatarPacket_Write(args.Packet.ovrNativePacket, size, data);
+
+            writer.Write(localSequence++);
+            writer.Write(size);
+            writer.Write(data);
+
+            packetData.Add(outputStream.ToArray());
+        }
+    }
+
+
+    private void DeserializeAndQueuePacketData(byte[] data)
+    {
+        using (MemoryStream inputStream = new MemoryStream(data))
+        {
+            BinaryReader reader = new BinaryReader(inputStream);
+            int remoteSequence = reader.ReadInt32();
+
+            int size = reader.ReadInt32();
+            byte[] sdkData = reader.ReadBytes(size);
+
+            System.IntPtr packet = Oculus.Avatar.CAPI.ovrAvatarPacket_Read((System.UInt32)data.Length, sdkData);
+            remoteDriver.QueuePacket(remoteSequence, new OvrAvatarPacket { ovrNativePacket = packet });
+        }
+    }
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            if (packetData.Count == 0)
+            {
+                return;
+            }
+
+            stream.SendNext(packetData.Count);
+
+            foreach (byte[] b in packetData)
+            {
+                stream.SendNext(b);
+            }
+
+            packetData.Clear();
+        }
+
+        if (stream.isReading)
+        {
+            int num = (int)stream.ReceiveNext();
+
+            for (int counter = 0; counter < num; ++counter)
+            {
+                byte[] data = (byte[])stream.ReceiveNext();
+
+                DeserializeAndQueuePacketData(data);
+            }
+        }
+    }
+
+
+    
+
+    public void OnJoinedRoom()
+    {
+        int viewId = PhotonNetwork.AllocateViewID();
+
+        PhotonNetwork.RaiseEvent(InstantiateVrAvatarEventCode, viewId, true, new RaiseEventOptions() { CachingOption = EventCaching.AddToRoomCache, Receivers = ReceiverGroup.All });
+    }
+
+
+    private void OnEvent(byte eventcode, object content, int senderid)
+    {
+        if (eventcode == InstantiateVrAvatarEventCode)
+        {
+            GameObject go = null;
+
+            if (PhotonNetwork.player.ID == senderid)
+            {
+                go = Instantiate(Resources.Load("LocalAvatar")) as GameObject;
+            }
+            else
+            {
+                go = Instantiate(Resources.Load("RemoteAvatar")) as GameObject;
+            }
+
+            if (go != null)
+            {
+                PhotonView pView = go.GetComponent<PhotonView>();
+
+                if (pView != null)
+                {
+                    pView.viewID = (int)content;
+                }
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
